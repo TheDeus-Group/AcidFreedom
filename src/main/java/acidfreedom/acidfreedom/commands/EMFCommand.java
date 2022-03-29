2cc4691dfc2bdb3201bf666869f9002f3cdbe2d4
Business Goose <arclicious@vivaldi.net>
2022-03-29 23:11:53 +0100

    Add usages & argument valiadtion

--- src/main/java/acidfreedom/acidfreedom/commands/EMFCommand.java
+++ src/main/java/acidfreedom/acidfreedom/commands/EMFCommand.java
@@ -10,6 +10,10 @@ import org.bukkit.entity.Player;
 public class EMFCommand implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if(args.length < 1) {
+            return false;
+        }
+
         Player target = Bukkit.getPlayer(args[0]);
         if(target != null) {
             target.sendMessage(ChatColor.RED + "ENGLISH MOTHERFUCKER, Do you speak it!?");
